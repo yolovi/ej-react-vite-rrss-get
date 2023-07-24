c60902d6a847ea6182778d8aac91a94a76be8422
yolovi <yolandalopevi@gmail.com>
2023-07-24 17:36:36 +0200

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,19 +17,28 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <div>
+      <p>
+        Investigación de React utilizando Vite completada!
+        </p>
+
+        <h2>Get con Axios</h2>
+        <button>Get user by name</button>
+
+
+      </div>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-        Investigación de React utilizando Vite completada!
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
